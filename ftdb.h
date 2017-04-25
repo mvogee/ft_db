e4fd80a02e2709581157933330d2af5b805b149d
Matthew Vogee <mvogee@yahoo.com>
2017-04-24 18:43:42 -0700

    first commit

---
+++ ftdb.h
@@ -0,0 +1,30 @@
+#ifndef FTDB_H
+# define FTDB_H
+
+# include <stdio.h>
+# include <unistd.h>
+
+typedef struct	s_headers
+{
+	t_headers	*next;
+	t_col		*col_head;
+	char		*header;
+	char		*datatype;
+}				t_headers
+
+typedef struct 	s_col
+{
+	t_col	*down;
+	t_col	*right;
+	char	*data;
+	char	*header;
+}				t_col;
+
+typedef struct	s_tables
+{
+	char		*table;
+	char		*filname;
+	t_tables	*next;
+}				t_tables;
+
+#endif
---
+++ main.c
@@ -0,0 +1,17 @@
+
+#include "ftdb.h"
+
+int		main(int argc, char const *argv[])
+{
+	// get user input from ac av.
+	// depending on what they give us is what we will do with what they give us
+	// create_table [table name]
+	// add_category [table][category]
+	// add_ellement	[table][name][data]
+	// delete_ellement [table][name]
+	// delete_category [table][category]
+	// delete_table	   [table]
+	// mod_ellement [table][element][category][new_data]
+	// get_data		(table)(element)(category)
+	return (0);
+}
